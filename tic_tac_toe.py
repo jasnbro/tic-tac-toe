8fde0866c571d1449a84da116f4c5332446b1b24
Jasmine <brooks.jasmine.n@gmail.com>
2020-08-11 20:47:19 -0500

    Modified gameboard display logic, restructured code, and began work on game logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,32 +1,31 @@
 import os
 
 user_input =  ''
-turn_cnt = 0
+turn_cnt = 1
 end_game = False
+is_digit = True
 game_board = ['' for x in range(9)]
+xo_marker = 'X' 
+iswinner = False
 
 #############
 # Functions 
 #############
 
-# Check input for valid  
-def check_input(user_input, info, cnt):
-    x = info
-    y = cnt
-
-    if user_input.upper() == 'Q' or user_input.upper() == 'QUIT':
-        x = True
-        print('Sorry to see you go. Thanks for playing!')
-    elif user_input.isdigit() and int(user_input) in range(0,10): 
-        y+=1
-    else:
-        print('Invalid Input. Please enter a number between 1 adnd 9 or enter Q to quit the game.')
-
-    return x, y
-
 # Display Game Board
 def display_game(board): 
-    print('board')
+    print(board[6:])
+    print(board[3:6])
+    print(board[:3])
+
+#Test Tic Tac Toe
+def game_logic(board):
+    a = board[6:]
+    b = board[3:6]
+    c = board[:3]
+
+    
+    
 
 #############
 # Main
@@ -34,7 +33,28 @@ def display_game(board):
 
 print("Let's Play Tic-Tac-Toe!")
 
-while end_game == False and turn_cnt < 9:
+display_game(game_board)
+
+while end_game == False and turn_cnt < 10:
+
+    if turn_cnt%2:
+        xo_marker = 'O'
+    else: 
+        xo_marker = 'X'
+    
     user_input = input("Player, your turn: ")
 
-    end_game, turn_cnt = check_input(user_input, end_game, turn_cnt)
+    if user_input.upper() in ['Q', 'QUIT', 'X', 'EXIT']:
+        end_game = True
+        print('Sorry to see you go. Thanks for playing!')
+    elif user_input.isdigit() and int(user_input) in range(0,10):
+        i = int(user_input)-1
+        game_board[i] = xo_marker
+        display_game(game_board)
+        turn_cnt+=1
+    else:
+        print('Invalid Input. Please enter a number between 1 adnd 9 or enter Q to quit the game.')
+    
+    #end_game, turn_cnt = check_input(user_input, end_game, turn_cnt, game_board)
+    
+    
