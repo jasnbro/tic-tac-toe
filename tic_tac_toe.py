1ade0a1615e62525b18c17e6c777268e4d32fc17
Jasmine <brooks.jasmine.n@gmail.com>
2020-08-16 14:43:44 -0500

    Refined game flow and began building out game logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,31 +1,66 @@
 import os
 
 user_input =  ''
+xo_marker = 'X'
 turn_cnt = 1
 end_game = False
-is_digit = True
-game_board = ['' for x in range(9)]
-xo_marker = 'X' 
 iswinner = False
+game_board = ['' for x in range(9)]
 
 #############
 # Functions 
 #############
 
-# Display Game Board
+# Displays Game Board
 def display_game(board): 
     print(board[6:])
     print(board[3:6])
     print(board[:3])
 
-#Test Tic Tac Toe
+# Tests if Tic Tac Toe winner has been found
 def game_logic(board):
     a = board[6:]
     b = board[3:6]
     c = board[:3]
+    
+    winner = True
 
+    if winner == False and game_over(board) == True:
+        print("Game Tied")
+        return True
+    if winner == False and game_over(board) == False:
+        display_game(board)
+        return False
+    else:
+        print("We have a winner!") 
+        return True
+
+# Determines whether to end the game based on user input
+def quit_game(user_input):
+    if user_input.upper() in ['Q', 'QUIT', 'X', 'EXIT']:
+        print('Sorry to see you go. Thanks for playing!')
+        return True
+    else: 
+        return False
+
+# Checks the game board to determine if all slots have been filled - meaning that the game is over
+def game_over(board):
+    if "" not in board:
+        return True
+    else: 
+        return False
+
+# Checks the user input for validity
+def valid_input(user_input):
+    if user_input in ['Q', 'QUIT', 'X', 'EXIT']:
+        return True
+    elif user_input.isdigit() and int(user_input) in range(1,10):
+        return True
+    else:
+        print("Invalid Input. Please enter a number between 1 adnd 9 or enter Q to quit the game. \n")
+        return False
     
-    
+
 
 #############
 # Main
@@ -35,26 +70,22 @@ print("Let's Play Tic-Tac-Toe!")
 
 display_game(game_board)
 
-while end_game == False and turn_cnt < 10:
+while end_game == False:
 
-    if turn_cnt%2:
-        xo_marker = 'O'
+    display_game(game_board)
+
+    xo_marker = ('O' if turn_cnt % 2 else 'X')
+
+    user_input = input("Player {}, your turn: ".format(xo_marker))
+
+    while valid_input(user_input) == False and game_over(game_board) == False: 
+            user_input = input("Player {}, your turn: ".format(xo_marker))
     else: 
-        xo_marker = 'X'
-    
-    user_input = input("Player, your turn: ")
-
-    if user_input.upper() in ['Q', 'QUIT', 'X', 'EXIT']:
-        end_game = True
-        print('Sorry to see you go. Thanks for playing!')
-    elif user_input.isdigit() and int(user_input) in range(0,10):
-        i = int(user_input)-1
-        game_board[i] = xo_marker
-        display_game(game_board)
-        turn_cnt+=1
-    else:
-        print('Invalid Input. Please enter a number between 1 adnd 9 or enter Q to quit the game.')
-    
-    #end_game, turn_cnt = check_input(user_input, end_game, turn_cnt, game_board)
-    
-    
+        if quit_game(user_input):
+            end_game = True
+        else:
+            game_board[int(user_input)-1] = xo_marker
+            end_game = game_logic(game_board)
+        
+print("\nThe winner is {} !".format(xo_marker))
+print("Thanks for playing, Goodbye!"))
